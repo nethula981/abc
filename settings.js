84a79b6c68f9815c2ad89c75c17803a834bdd04f
nethula981 <nethula981@gmail.com>
2026-01-17 21:14:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,5 +7,5 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'df2c8f92844ceb0e' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+SESSION_NAME: process.env.PORT === undefined ? "asithamd" : process.env.SESSION_NAME,
 };
